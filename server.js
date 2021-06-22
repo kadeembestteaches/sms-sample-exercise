21a8cb9c750c9b31b472c541da3add829dd8dbc2
Kadeem Best <info@theweekendhack.com>
2021-06-22 15:14:10 -0400

    Added my DOTENV file

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require("express");
 const exphbs = require("express-handlebars")
 const bodyParser = require('body-parser');
 
-const GeneralController = require("./controllers/GeneralController.js")
+const GeneralController = require("./controllers/GeneralController.js");
+
+//This is to tell express to where to locate the ENV File
+
+
+//If the app is running within Heroku, DON"T try to load keys.env
+if(process.env.NODE_ENV !="production")
+{
+    require("dotenv").config({path :"config/Keys.env"});
+}
+
 
 const app = express();
 
@@ -18,8 +28,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use("/",GeneralController);
 
-const PORT = 3000;
 
-app.listen(PORT,()=>{
+app.listen(process.env.PORT,()=>{
     console.log(`Web Server is up and running on PORT : ${PORT}`)
 });
